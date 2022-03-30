398f64d95da0082e57e3fe4b5ba7e4f453a52f62
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-30 16:09:10 -0400

    Changed local settings and added serializers.py

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -113,6 +113,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try: 
-    from cars_project.local_setting import *
+    from cars_project.local_settings import *
 except ImportError:
     pass
