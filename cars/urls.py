3579da925db33f98719e76c8c3ff62700b16da87
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-30 18:38:11 -0400

    Postman set up and views.py cars import and Serializer import for cars

--- cars/urls.py
+++ cars/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cars/', views.cars_list),
+    path('', views.cars_list),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,14 @@
+import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
 
+from cars import serializers
 @api_view(['GET'])
 def cars_list(request):
+    cars = Car.objects.all()
 
+    serializer = CarSerializer(cars, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
